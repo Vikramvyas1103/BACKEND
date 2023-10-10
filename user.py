fec4e625155cc93995fd19b77ce5f857c25fd1b1
Vikramvyas1103 <144376520+Vikramvyas1103@users.noreply.github.com>
2023-10-10 14:52:47 +0530

    Add files via upload

--- user.py
+++ user.py
@@ -1,70 +1,17 @@
-from fastapi import APIRouter ,HTTPException
-from models.user import User , validate_email,validate_password
-from config.db import conn 
-from schemas.user import userEntity , usersEntity
-from bson import ObjectId
-user = APIRouter() 
+def userEntity(item) -> dict:
+    return {
+        "id":str(item["_id"]),
+        "name":item["name"],
+        "email":item["email"],
+        "password":item["password"]
+    }
+
+def usersEntity(entity) -> list:
+    return [userEntity(item) for item in entity]
 
 
-@user.get('/')
-async def find_all_users():
-    return usersEntity(conn.Testify.Organisation.find())
+#def serializeDict(a) -> dict:
+    #return {**{i:str(a[i]) for i in a if i=='_id'},**{i:a[i] for i in a if i!='_id'}}
 
-@user.get('/{id}')
-async def find_one_user(id):
-    document = conn.Testify.Organisation.find_one({"_id":ObjectId(id)})
-    if document is None: # Adding the http exception to check the errors 
-        raise HTTPException(status_code=404, detail="User not found")
-    return userEntity(document)
-
-@user.post('/')
-async def create_user(user: User):
-    try:
-        # Validate email and password
-        validated_email = validate_email(user.email)
-        validated_password = validate_password(user.password)
-
-        # Create a new user document with validated data
-        user_data = dict(user)
-        user_data['email'] = validated_email
-        user_data['password'] = validated_password
-
-        # Insert the user data into the database
-        _id = conn.Testify.Organisation.insert_one(user_data)
-
-        # Return a success response
-        document = userEntity(conn.Testify.Organisation.find_one({"_id": _id.inserted_id}))
-        return {"status": "ok", "data": document}
-    except ValueError as e:
-        # Handle validation errors and return an appropriate response
-        return {"status": "error", "message": str(e)}
-
-
-@user.put('/{id}')
-async def update_user(id,user: User):
-    try:
-        # Validate email and password
-        if user.email:
-            user.email = validate_email(user.email)
-        if user.password:
-            user.password = validate_password(user.password)
-
-        # Update the user data in the database
-        conn.Testify.Organisation.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(user)})
-
-        # Return a success response
-        document = userEntity(conn.Testify.Organisation.find_one({"_id": ObjectId(id)}))
-        return {"status": "ok", "data": document}
-    except ValueError as e:
-        # Handle validation errors and return an appropriate response
-        return {"status": "error", "message": str(e)}
-
-@user.delete('/{id}')
-async def delete_user(id):
-    try:
-        # Delete the user data from the database
-        document = userEntity(conn.Testify.Organisation.find_one_and_delete({"_id": ObjectId(id)}))
-        return {"status": "ok", "data": document}
-    except Exception as e:
-        # Handle any exceptions that may occur during deletion
-        return {"status": "error", "message": str(e)}
+#def serializeList(entity) -> list:
+    #return [serializeDict(a) for a in entity]
